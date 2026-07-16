b41bef88f03cea0f611bbeda86af341813af1720
Chili888 <lajiao645@gmail.com>
2026-07-16 17:49:55 +0700

    Host storefront on production server

---
+++ deploy/frontend/config.production.js
@@ -0,0 +1,6 @@
+window.APP_CONFIG = Object.freeze({
+  supabaseUrl: "https://lgkwdjuzpatwnpyfzjnk.supabase.co",
+  supabaseAnonKey: "sb_publishable_gnldHTnXho6TnD2CBXY6tw_tEE0ZQtR",
+  pageUrl: "https://bot.cverseintl.cloud/",
+  apiBaseUrl: "https://bot.cverseintl.cloud"
+});
